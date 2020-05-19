6dc5a33300bd83a6bfb12729a49b013510abedeb
Adrien Lafage <adrienlafage@outlook.com>
2020-05-19 19:31:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name='gym_platformer',
+    name='GymPlatformer',
     version='1.0',
     author='Aydens01',
     author_email='adrienlafage36@gmail.com',
     description='Platformer environment package',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['gym', 'pygame']
+    install_requires=['gym', 'pygame'],
+    packages=["gymplatformer"]
 )
