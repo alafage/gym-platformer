bdf4ba2ac6137fb62d311848ebbc9ea45be592a7
lafageadri <lafageadri@eisti.eu>
2020-05-19 21:07:10 +0200

    Update repository architecture

--- gymplatformer/__init__.py
+++ gymplatformer/__init__.py
@@ -1,7 +1,7 @@
 from gym import make as gym_make
 
-from .envs.discrete_platformer_env import DiscretePlatformerEnv
-from .envs.platformer_env import PlatformerEnv
+from .discrete_platformer_env import DiscretePlatformerEnv
+from .platformer_env import PlatformerEnv
 
 def make(env_name, *make_args, **make_kwargs):
     if env_name == "PlatformerEnv":
